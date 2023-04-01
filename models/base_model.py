ffb7080cacdd4d71c14fb4af6a244609bfa7a984
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-01 11:54:19 -0700

    done

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Class BaseModel """
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel:
+    """ construct """
+
+    def __init__(self, *args, **kwargs):
+        """ Construct """
+        if kwargs:
+            if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+            if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+            if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """print: [<class name>] (<self.id>) <self.__dict__>"""
+        return('[' + type(self).__name__ + '] (' + str(self.id) +
+               ') ' + str(self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        aux_dict = self.__dict__.copy()
+        aux_dict['__class__'] = self.__class__.__name__
+        aux_dict['created_at'] = self.created_at.isoformat()
+        aux_dict['updated_at'] = self.updated_at.isoformat()
+        return aux_dict
