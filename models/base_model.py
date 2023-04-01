28bf5886ff0d63d9143f2e94e95db6fddef0efcf
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-01 12:12:30 -0700

    done

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from uuid import uuid4
 
+
 class BaseModel:
     """ construct """
 
@@ -34,11 +35,11 @@ class BaseModel:
                ') ' + str(self.__dict__))
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """return a dictionary with keys/values of __dict__ of the instance"""
         aux_dict = self.__dict__.copy()
         aux_dict['__class__'] = self.__class__.__name__
         aux_dict['created_at'] = self.created_at.isoformat()
