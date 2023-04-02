398e5b924dfacbdf06493bc9b312b6e0395fd21f
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-02 05:15:24 -0700

    done

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ Class BaseModel """
 from datetime import datetime
 from uuid import uuid4
+import models
 
 
 class BaseModel:
@@ -28,6 +29,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
@@ -37,6 +39,7 @@ class BaseModel:
     def save(self):
         """updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """return a dictionary with keys/values of __dict__ of the instance"""
