fb4b095c17eabfaa220e44b05b9b68b60d0a8412
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-05-07 22:57:46 -0700

    done

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for the city class"""
+import pep8
+import unittest
+from models.city import City
+
+
+class TestBaseModelpep8(unittest.TestCase):
+    """test pep8"""
+    def test_pep8(self):
+        """test for city file and test_city file"""
+        style = pep8.StyleGuide(quiet=True)
+        city_pep8 = "models/city.py"
+        test_city_pep8 = "tests/test_models/test_city.py"
+        result = style.check_files([city_pep8, test_city_pep8])
+        self.assertEqual(result.total_errors, 0)
