df2fe92ba52dd24e990736eecbbcd6d8fb876a34
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-05-09 02:49:03 -0700

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,8 @@ from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """serializes and deserializes instances to and from JSON file
